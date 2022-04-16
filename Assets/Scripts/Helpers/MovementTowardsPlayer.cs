7d5e17a83a4c22f60f3856ff94fc287a84def882
Héctor Quiñones <hector.quinones@enroutesystems.com>
2022-04-16 17:22:09 -0600

    Added simple enemy movement

---
+++ Assets/Scripts/Helpers/MovementTowardsPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTowardsPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public Transform target;
+    private float timeRemaining = 3;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+       if(timeRemaining > 0 && target.position.x < transform.position.x) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            timeRemaining -= Time.deltaTime;
+        } else {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0), Space.World);
+        }
+    }
+}
