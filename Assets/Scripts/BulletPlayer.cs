c0027b5769c84b2331cb82eb6910e07b66e30d5a
Miguel Bui <miguelbuir@gmail.com>
2022-07-03 17:31:13 -0600

    bullet

--- Assets/Scripts/BulletPlayer.cs
+++ Assets/Scripts/BulletPlayer.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class BulletPlayer : MonoBehaviour
 {
-    private float _speed = 180;
+    private float speed = 180;
     
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
+        Move();
+    }
+
+    private void Move(){
         transform.Translate(new Vector3(_speed * Time.deltaTime, 0, 0), Space.World);
         if(transform.position.x >= 35){
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Enemy"){
             Destroy(gameObject);
         }
     }
